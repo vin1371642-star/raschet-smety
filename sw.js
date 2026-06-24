ef3076f1708e952a54faf36d93e63336329c6e20
vin1371642-star <vin1371642@gmail.com>
2026-06-24 19:34:12 +0300

    feat: № сметы=обращение, № накладной=договор; накладная — дата вверху + подписи продавца/покупателя; шрифт 1:1 с EXE

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'smeta-6055887adc0e';
+const CACHE = 'smeta-fcc4546834dc';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   self.skipWaiting();
