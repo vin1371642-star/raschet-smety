df739008f725877b09b88cd6b2b17dc915d05190
vin1371642-star <vin1371642@gmail.com>
2026-06-24 12:18:23 +0300

    update prices 2026-06-24 12:18

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'smeta-31c780bd2214';
+const CACHE = 'smeta-06da251b3940';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   self.skipWaiting();
